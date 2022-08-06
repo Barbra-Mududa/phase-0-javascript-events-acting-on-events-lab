2a7e2dd284d33d56e60e4d2225f7316540e85c53
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-07 00:36:04 +0300

    Completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+// //change color
+// dodger.style.backgroundColor = "#000000";
+// //improve visibility
+// dodger.style.backgroundColor = "#FF69B4";
+// //Move element up
+// dodger.style.bottom = "100px";
+
+// //resetting to original position
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+
